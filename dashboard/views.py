14cd280496ae43dd55f3a66cf95955961c0d9f94
magickoo <magickhu@gmail.com>
2024-06-20 17:04:02 +0530

    feat: complete Interface Build-up

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def staff(request):
     return render(request,'dashboard/staff.html')
+
+def clients(request):
+    return render(request,'dashboard/clients.html')
+
+def bookings(request):
+    return render(request,'dashboard/bookings.html')
